580c554ee8629df84ec8b59a5f95524d26a30728
Reyan chauhan <reyancopy@gmail.com>
2025-09-10 02:31:06 +0530

    Q3

--- 100daysofcode/Q1-Q10/Q3.c
+++ 100daysofcode/Q1-Q10/Q3.c
@@ -3,14 +3,14 @@
 #include<stdio.h>
 int main()
 {
-float l,b,perimeter,area;
+float l,b,perimeter,areaa;
 printf("enter the length:");
 scanf("%f",&l);
 printf("enter the breadth:");
 scanf("%f",&b);
 perimeter=2*(l+b);
 printf("perimeter=%fm\n",perimeter);
-area=l*b;
-printf("area=%fm^2\n",area);
+areaa=l*b;
+printf("areaa=%fm^2\n",areaa);
 return 0;
 }
