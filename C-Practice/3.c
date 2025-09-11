47ca002f9b040e5cbb1eec5d9efc74318c05d5b7
Reyan chauhan <reyancopy@gmail.com>
2025-09-12 00:14:53 +0530

    3

---
+++ C-Practice/3.c
@@ -0,0 +1,12 @@
+// wap to find the area of circle.
+#include<stdio.h>
+
+int main()
+{
+    float radius,area;
+    printf("enter the radius of circle");
+    scanf("%f",&radius);
+    area= 3.14*(radius*radius);
+    printf("area of circle: %f \n",area);
+    return 0;
+}
