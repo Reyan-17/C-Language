bcb55dd24738be9b67df8b41347a50dde3df0173
Reyan chauhan <reyancopy@gmail.com>
2025-09-11 23:20:22 +0530

    1

---
+++ C-Practice/1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("enter the first no. :");
+    scanf("%d",&a);
+    printf("enter the second no. :");
+    scanf("%d",&b);
+    printf("sum is %d\n",a+b);
+    return 0;
+}
