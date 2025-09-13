abe01fb9288fb7158429eed8dfae33557e5433f4
Reyan chauhan <reyancopy@gmail.com>
2025-09-13 06:09:27 +0530

    6

--- 100daysofcode/Q1-Q10/Q6.c
+++ 100daysofcode/Q1-Q10/Q6.c
@@ -1,2 +1,14 @@
 //Q6: Write a program to swap two numbers using a third variable.
 
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the first no.:");
+    scanf("%d",&a);
+    printf("enter the second no.:");
+    scanf("%d",&b);
+    c=a;a=b;b=c;
+    printf("Ater Swap:a=%d b=%d",a,b);
+    return 0;
+}
