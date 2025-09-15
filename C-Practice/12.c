fe287c07101d8a5c6d92122dd469344f874f867e
Reyan chauhan <reyancopy@gmail.com>
2025-09-16 04:36:09 +0530

    12

---
+++ C-Practice/12.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the length of 3 sides of triangle:");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a==b&&b==c&&c==a)
+    {
+        printf("Its an equilateral triangle\n");
+    }
+    else if(a==b||b==c||c==a)
+    {
+        printf("Its an isosceles\n");
+    }
+    else
+    {
+        printf("Its a scalene triangle\n");
+    }
+    return 0;
+}
