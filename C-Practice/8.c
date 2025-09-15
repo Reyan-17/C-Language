5efbe84039b16da7476783c1336e4b0504f45986
Reyan chauhan <reyancopy@gmail.com>
2025-09-16 03:13:38 +0530

    8

---
+++ C-Practice/8.c
@@ -0,0 +1,10 @@
+// input any character and display its ASCII value.
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("enter the character:");
+    scanf("%c",&ch);
+    printf("ASCII value of %c is %d\n",ch,ch);
+    return 0; 
+}
