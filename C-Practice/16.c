fff533cbe0e017c4c3ab0ca6297feaa1196385a3
Reyan chauhan <reyancopy@gmail.com>
2025-09-20 03:33:18 +0530

    16

---
+++ C-Practice/16.c
@@ -0,0 +1,10 @@
+//use of ternary operator.
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("enter the age ");
+    scanf("%d",&x);
+    x >=18?printf("U are an adult\n"):printf("U are not an adult\n");
+    return 0;
+}
