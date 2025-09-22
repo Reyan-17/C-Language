97419adc5640470a982c1ba63775cd293790f731
Reyan chauhan <reyancopy@gmail.com>
2025-09-22 07:41:56 +0530

    19

---
+++ C-Practice/19.c
@@ -0,0 +1,10 @@
+//wap to print average of two numbers.
+#include<stdio.h>
+int main()
+{
+    float a,b,c;
+    printf("enter three numbers:");
+    scanf("%f%f%f",&a,&b,&c);
+    printf("average = %f",(a+b+c)/3);
+    return 0;
+}
